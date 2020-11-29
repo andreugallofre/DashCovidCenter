fb1fd6260293da737aa9093e3199f9ef7dd8e120
Carlota Catot <carlota.catot@hp.com>
2020-11-29 09:49:50 +0100

    added contact

--- client/src/components/Contact/Contact.js
+++ client/src/components/Contact/Contact.js
@@ -1,27 +1,30 @@
 import React, {Component} from 'react';
-import { Row, Col, Layout, Divider, Form, Input, Button } from 'antd';
+import { Row, Col, Layout, Divider, Typography } from 'antd';
 import './Contact.css';
+
+const { Title } = Typography;
 export class Contact extends Component {
 
     render() {
         return(
             <Layout>
-                <h1>Contacto</h1>
+                <Title>Contacto</Title>
                 <Row>
                     <Col span={12}>
-                    <Form name="contact-form" netlify>
-                        <Form.Item rules={[{ type: 'email', required: true }]}>
-                            <Input style={{ width: 160 }} placeholder="Correo electronico" />
-                        </Form.Item>
-                        <Form.Item>
-                            <Input.TextArea style={{ width: 300 }} placeholder="Consulta a realizar"/>
-                        </Form.Item>
-                        <Form.Item>
-                            <Button type="primary" htmlType="submit">
-                                Enviar
-                            </Button>
-                        </Form.Item>
-                    </Form>
+                        <form name="contact" method="POST" data-netlify="true">
+                            <p>
+                                <input type="text" name="name" placeholder="Name (optional)"/>
+                            </p>
+                            <p>
+                                <input type="email" name="email" placeholder="Email (optional)"/>
+                            </p>
+                            <p>
+                                <textarea name="message" placeholder="Write here your suggestions, either a speaker or topics for future T&W. We are open to everything!" required></textarea>
+                            </p>
+                            <p>
+                                <button type="submit" class="form-button">Enviar</button>
+                            </p>
+                        </form>
                     </Col>
                     <Divider type="vertical" />
                     <Col span={12}>
